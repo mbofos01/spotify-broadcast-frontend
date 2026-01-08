652adc74c1ac242ce6c9aece39750bbc562508fe
Michail-Panagiotis Bofos <49307193+mbofos01@users.noreply.github.com>
2026-01-08 20:56:37 +0200

    Added Emojis

--- src/components/NothingPlayingCard.js
+++ src/components/NothingPlayingCard.js
@@ -3,14 +3,20 @@ import React, { useMemo } from "react";
 function NothingPlayingCard() {
   const randomMessage = useMemo(() => {
     const messages = [
-      "Have you checked out my playlists?",
-      "Awfully quiet around here...",
-      "Probably I'm asleep and honestly why aren't you?",
-      "Enjoying the sound of silence",
-      "Every 60 Seconds in Earth a Minute Passes",
-      "Yes you should pass me the AUX. Always"
+      "Have you checked out my playlists? 👉🏼👈🏼",
+      "Awfully quiet around here... 😶🦗",
+      "Enjoying the Sound of Silence 🌙🎵",
+      "Every 60 Seconds in Earth a Minute Passes 🌍⏳",
+      "Yes, you should pass me the AUX. Always 😎🎶"
   
     ];
+    const currentHour = new Date().getHours();
+
+    // Special morning message between 3 AM and 7 AM
+    if (currentHour >= 3 && currentHour <= 7) {
+      return "Probably I'm asleep and honestly why aren't you? 😪😴";
+    }
+
     return messages[Math.floor(Math.random() * messages.length)];
   }, []);
 
